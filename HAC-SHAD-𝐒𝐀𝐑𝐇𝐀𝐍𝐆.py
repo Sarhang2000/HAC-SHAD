e5b974b2e319d7e0673b337811d8a0016db96634
𝐒𝐀𝐑𝐇𝐀𝐍𝐆 <waaw1387@gmail.com>
2024-10-06 15:34:22 +0330

    Add files via upload

---
+++ HAC-SHAD-𝐒𝐀𝐑𝐇𝐀𝐍𝐆.py
@@ -0,0 +1,63 @@
+from os import system
+from time import sleep
+from colorama import Fore
+import sys
+import random
+
+system("clear")
+
+bnr = (Fore.LIGHTGREEN_EX+f"""
+
+   ID_Telegram = @SARHANG_GOD_2
+  
+   ID_Channel-telegram = @SARHANG_HACKER
+
+
+
+
+                         .;ldk0XNWWWWWWMMNXOxl,
+                        :k0KXXNNNNWWWWWWMMMMMMMMW0c
+                       odkKXXXNNNNWWWWWWMMMMMMMMWWWK
+                      'dc;'..':oxKNWWWWWNNX0xold0WWWd
+                      l,..,'....':oONN0o;'::lkOOdlkWW
+                      d:x0KXX0d:':kKNNKc:x0NWWWMMWKWW.
+                      xxOOolcldkd..KWMKd0KKOkO00WWWWW;
+                      klo;:....'cccOWMWWOl'.  .oKNNWWo
+                      xkKXK0KXXNNKd0WMMMMWWWWWWWWWWNW0
+                      kKXXXNNNWWNOxKWMMMMMMMMMMMMMMMWX
+                      xxOO0KKKOOKOdXWMMMMMNNWWWWWWWWNk
+                      llc;c:ldkdo:ld0KXKkNWK0OOO0KXNN;
+                      ::l.xXXKOxc ..'.'xXWMMMMK,:kXNN
+                       ;lcl.'                 .dKNNN:
+                        ;lllodxxdoxxxkxdoO0O00XXWNX0
+                         ::dccllccoodxxOOkKXXKNWXX0
+                           ;dxok0K0l  dXNNWNNWNXX
+                             :OxOXNo  xWWWWWNNl
+                               lx0Xl  dWWWWx
+                                 .xx 'XNk
+                                 
+                                 
+                                 
+                                 
+""")
+for i in bnr:
+  sys.stdout.write(i)
+  sys.stdout.flush()
+  sleep(0.001)
+print("")
+name = input(Fore.LIGHTYELLOW_EX+""" Target number ....?  """)
+
+system("clear")
+
+bag= (f"KOD SHAD --->","KOD SHAD --->")
+
+bag_1= (random.choice(bag))
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9 ,0 ]
+
+start_number = str(random.randint(0, 9))
+
+result = start_number + '‌' + '‌'.join([str(random.choice(numbers)) for _ in range(6)])
+
+
+print(f"\033[31m{bag_1}.\033[32m{result}")
